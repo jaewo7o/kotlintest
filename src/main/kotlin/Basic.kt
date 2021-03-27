0d7653ca50ece1cffc6b65ce0b344edecc121d5b
jaewoo <jaewo7o@naver.com>
2021-03-27 21:59:24 +0900

    클래스 추가

--- src/main/kotlin/Basic.kt
+++ src/main/kotlin/Basic.kt
@@ -3,12 +3,14 @@ fun main() {
     print("Hello World")
 
     // 변수형변환
+    println("## type change")
     var a: Int = 123
     var b: Long = a.toLong()
     var c = 123 // 타입추론기능 사용
     print(c)
 
     // 배열
+    println("## array")
     var intArr = arrayOf(1, 2, 3, 4, 5)
     var nullArr = arrayOfNulls<Int>(5)
 
@@ -16,27 +18,89 @@ fun main() {
     println(intArr[3])
 
     // 함수
+    println("## function")
     println(add(1, 2, 3))
     println(add2(1, 2, 3))
 
     // 조건문
+    println("## condition")
     if (a > 10) {
         println("a는 10보다 크다")
     } else {
         println("a는 10보다 작다.")
     }
 
-    doWhen(a)
+    doWhen(a);
+
+    // 반복문
+    println("## loop")
+    for (i in 1..9) {
+        if (i == 3) break
+        println(i)
+    }
+
+    loop@ for (i in 1..10) {
+        for (j in 1..10) {
+            if (i == 1 && j == 2) break@loop
+            println("i : $i, j: $j")
+        }
+    }
+
+    println("## class")
+    var persona = Person("이름1", 1990)
+    println("안녕하세요, ${persona.birthYear}년생 ${persona.name}입니다.")
+    persona.introduce()
+
+    var personb = Person("이름2")
+    personb.introduce()
+
+    var dog = Dog("녀석이", 15)
+    dog.bark()
+    dog.eat()
+}
+
+open class Animal(var name: String, var age: Int, var type: String) {
+    fun introduce() {
+        println("저는 ${type} ${name}이고, ${age}살 입니다.")
+    }
+
+    open fun eat() {
+        println("음식을 먹습니다.")
+    }
+}
+
+class Dog(name: String, age: Int) : Animal(name, age, "개") {
+    fun bark() {
+        println("멍멍")
+    }
+
+    override fun eat() {
+        println("개밥을 먹습니다.")
+    }
+}
+
+class Person(var name: String, val birthYear: Int) {
+    init {
+        println("name : ${name}")
+    }
+
+    constructor(name: String) : this(name, 1990)
+
+    fun introduce() {
+        println("안녕하세요, ${birthYear}년생 ${name}입니다.")
+    }
 }
 
 fun doWhen(a: Any) {
-    when (a) {
-        1 -> println("정수 1입니다.")
-        "Jaewoo" -> println("이름이 입력되었습니다.")
-        is Int -> println("Int 형이 입력되었씁니다.")
-        !is String -> println("문자열이 아닙니다.")
-        else -> println("해당사항이 없습니다.")
+    var result = when (a) {
+        1 -> "정수 1입니다."
+        "Jaewoo" -> "이름이 입력되었습니다."
+        is Int -> "Int 형이 입력되었씁니다."
+        !is String -> "문자열이 아닙니다."
+        else -> "해당사항이 없습니다."
     }
+
+    println(result)
 }
 
 fun add(a: Int, b: Int, c: Int): Int {
---
+++ src/main/kotlin/java/Book.java
@@ -0,0 +1,30 @@
+package java;
+
+public class Book {
+    private String name;
+    private String author;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Book() {
+    }
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+}
